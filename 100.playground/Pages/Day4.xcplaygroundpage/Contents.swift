17b2ff7da845445178702ecd247031ed54ae566b
Eric Ciccotti <eciccotti@aubay.fdj.com>
2023-08-06 21:49:14 +0200

    feat: Day 4 / 100

--- 100.playground/Pages/Day4.xcplaygroundpage/Contents.swift
+++ 100.playground/Pages/Day4.xcplaygroundpage/Contents.swift
@@ -1,7 +1,7 @@
-//: [Previous](@previous)
+import Cocoa
 
-import Foundation
+var fruits: [String] = ["orange","banana","kiwi","kiwi","orange"]
+var uniqueItemsInFruits = Set<String>(fruits)
 
-var greeting = "Hello, playground"
-
-//: [Next](@next)
+print("Number of items in fruits's array = \(fruits.count)")
+print("There is \(uniqueItemsInFruits.count) unique items in this array")
