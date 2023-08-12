442ff5fd87476f21d817fde92675b9e4eb8ce0bf
Eric Ciccotti <eciccotti@aubay.fdj.com>
2023-08-12 02:32:55 +0200

    feat: Day 7 - hard

--- 100.playground/Pages/Day7.xcplaygroundpage/Contents.swift
+++ 100.playground/Pages/Day7.xcplaygroundpage/Contents.swift
@@ -7,3 +7,58 @@ func multiPrint(sentence: String, times: Int) {
 }
 
 multiPrint(sentence: "Jony", times: 5)
+
+func isContainSameLetters(a: String, b: String) -> String {
+    var itContain = Bool()
+    a.sorted()
+    b.sorted()
+    itContain = a == b && a.count == b.count
+    return itContain ? "it contain same letters !" : "it doesn't contain same letters"
+}
+
+print(isContainSameLetters(a: "Join", b: "ji"))
+
+func pythagoras(a: Double, b: Double) -> Double {
+    return sqrt(a * a + b * b)
+}
+
+print(pythagoras(a: 3, b: 4))
+
+//SET
+//for speed gain and
+//if order doesn't matter and
+//can't contain duplicate
+let uniqueDukeWeapons = Set<String>(["shotgun","uzi","machine gun","rpg","rpg"])
+
+
+//dictionnary
+// order doesn't matter
+let fruits = ["banane":"jaune", "kiwi":"vert", "fraise":"rouge"]
+
+//tuple
+//order matter
+let playerBar = (myList: "weaponsList", setOfWeapons: [1,2,3], cool: true)
+print(playerBar.myList)
+
+//function without extern parameter
+//with the _ no need extern parameter
+func whoAreYou(_ name: String) -> String {
+    "the real \(name)"
+}
+let myNameIs: String = "Slim Shady !"
+let result = whoAreYou(myNameIs)
+print(result)
+
+//function with ARGUMENT LABEL
+//function with external parameter "foodILove"
+//internal parameter "food"
+func foodLover(foodIlove food: String) -> String {
+    "I like eat \(food)"
+}
+
+foodLover(foodIlove: "pizza")
+
+
+        
+
+
