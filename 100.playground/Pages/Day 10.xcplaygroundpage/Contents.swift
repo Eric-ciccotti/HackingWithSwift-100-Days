31bc9d92756fdfebd4d5abee4aa8081edf1a9a94
Eric Ciccotti <=>
2023-08-16 18:21:49 +0200

    feat: Day 10

--- 100.playground/Pages/Day 10.xcplaygroundpage/Contents.swift
+++ 100.playground/Pages/Day 10.xcplaygroundpage/Contents.swift
@@ -1,2 +1,48 @@
 import Cocoa
 
+struct Personnage {
+    var name: String
+    var age: Int
+    
+    mutating func setName(newName: String) {
+        name = newName
+    }
+    
+    func sayHello() {
+        print("Hello " + name)
+    }
+    
+
+}
+
+var Jone = Personnage(name:"Jone",age: 10)
+Jone.sayHello()
+
+///
+///
+struct Employee {
+    let name: String
+    var vacationAllocated = 14
+    var vacationTaken = 0
+    
+    var vacationRemaining: Int {
+        get {
+            vacationAllocated - vacationTaken
+        }
+
+        set {
+            vacationAllocated = vacationTaken + newValue
+        }
+    }
+}
+
+var jonybonobo = Employee(name: "Jony bonbo soft")
+jonybonobo.vacationTaken = 10
+
+print(jonybonobo.vacationRemaining)
+
+jonybonobo.vacationRemaining = 10
+
+print(jonybonobo.vacationRemaining)
+    
+
