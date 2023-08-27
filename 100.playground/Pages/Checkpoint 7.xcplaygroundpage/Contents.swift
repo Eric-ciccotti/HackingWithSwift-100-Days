736f3c4eddbc0392c88b76f3f60e433204c1bed2
Eric Ciccotti <=>
2023-08-27 18:32:22 +0200

    exercise finished

--- 100.playground/Pages/Checkpoint 7.xcplaygroundpage/Contents.swift
+++ 100.playground/Pages/Checkpoint 7.xcplaygroundpage/Contents.swift
@@ -1,7 +1,54 @@
-//: [Previous](@previous)
+import Cocoa
 
-import Foundation
+class Animal {
+    var legs: Int
+    init(legs: Int) {
+        self.legs = legs
+    }
+}
 
-var greeting = "Hello, playground"
+class Dog : Animal {
+    func speak() {
+        print("wouaf wouaf !!")
+    }
+}
 
-//: [Next](@next)
+class Cat : Animal {
+    var isTame: Bool
+    func speak() {
+        print("miaou miaou !!")
+    }
+    init(legs: Int, isTame: Bool) {
+        self.isTame = isTame
+        super.init(legs: legs)
+    }
+}
+
+class Corgi : Dog {
+    override func speak() {
+        print("corgin corgin corgi..!")
+    }
+}
+
+class Poodle : Dog {
+    override func speak() {
+        print("huaf huaf huaf...!")
+    }
+}
+
+class Persian : Cat {
+    override func speak() {
+        print("miiiou miiiou..!")
+    }
+}
+
+class Lion : Cat {
+    override func speak() {
+        print("wraaaah...wrahhh !")
+    }
+}
+
+var jonbig = Lion(legs: 4, isTame: false)
+jonbig.speak()
+let persia = Persian(legs: 4, isTame: true)
+persia.speak()
