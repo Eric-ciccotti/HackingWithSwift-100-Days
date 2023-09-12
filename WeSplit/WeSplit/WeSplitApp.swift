5f06121752110e3695b8885c68cd0a7964834a18
Eric Ciccotti <=>
2023-09-12 22:47:18 +0200

    feat: Wesplit - first real project

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by P994396 on 12/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
