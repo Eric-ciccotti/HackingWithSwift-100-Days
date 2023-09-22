e615a31175474e6f351592ff14d4051b0cdbc0d0
Eric Ciccotti <eciccotti.aubay@lfdj.com>
2023-09-23 01:22:53 +0200

    feat: projet 1 finished - time converter

---
+++ CoolUnitConverter/CoolUnitConverter/CoolUnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolUnitConverterApp.swift
+//  CoolUnitConverter
+//
+//  Created by P994396 on 21/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoolUnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
