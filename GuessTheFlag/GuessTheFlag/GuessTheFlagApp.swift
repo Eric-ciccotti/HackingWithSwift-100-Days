eedb5f1faf2c59881c78165ff79dbba61d1bc51d
Eric Ciccotti <eciccotti.aubay@lfdj.com>
2023-09-28 19:07:59 +0200

    feat: projet 2 finished - guess flag

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by P994396 on 23/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
